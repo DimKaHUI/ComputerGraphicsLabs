010f3ca90cac4fe02f389a2283763ac4d9654a4e
DimKaHUI <wet75787@tqosi.com>
2018-05-29 18:28:59 +0300

    Lab_09 implemented

---
+++ ComputerGraphicsVS/Lab_09/Cutter.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_09
+{
+    static class Cutter
+    {
+        struct Vector
+        {
+            public int x;
+            public int y;
+            public int z;
+
+            public Vector(int a, int b, int c = 0)
+            {
+                x = a;
+                y = b;
+                z = c;
+            }
+            public Vector(Point end, Point start)
+            {
+                x = end.X - start.X;
+                y = end.Y - start.Y;
+                z = 0;
+            }
+            public static void VectorMult(Vector a, Vector b, ref Vector res)
+            {
+                res.x = a.y * b.z - a.z * b.y;
+                res.y = a.z * b.x - a.x * b.z;
+                res.z = a.x * b.y - a.y * b.x;
+            }
+        };
+
+        public static int GetDirection(List<Point> poligon)
+        {
+            // Если знаки всех в-орных произведений смежных сторон равны 0,
+            // то прямоуг - отрезок
+            // Если есть и положит. и  отриц., то многоуг невыпуклый
+            // Если все неотрицательные, то выпуклый и нормали напр. влево
+            // Если все неположительны, то выпуклый и нормали напр. вправо
+
+            Vector a = new Vector(poligon[1], poligon[0]);
+            Vector b = new Vector();
+            Vector tmp = new Vector();
+            int n = poligon.Count;
+
+            int res = 0;
+
+            for (int i = 1; i < n - 1; i++)
+            {
+                b = new Vector(poligon[i + 1], poligon[i]);
+                Vector.VectorMult(a, b, ref tmp);
+
+                if (res == 0)
+                    res = Math.Sign(tmp.z);
+
+                if (res != Math.Sign(tmp.z))
+                {
+                    if (tmp.z != 0)
+                        return 0;
+                }
+                a = b;
+            }
+
+            b = new Vector(poligon[1], poligon[0]);
+            Vector.VectorMult(a, b, ref tmp);
+
+            if (res == 0)
+                res = Math.Sign(tmp.z);
+
+            if (res != Math.Sign(tmp.z))
+            {
+                if (tmp.z != 0)
+                    return 0;
+            }
+
+            return res;
+        }
+
+        private static int VectorMultZ(int ax, int ay, int bx, int by) //векторное произведение
+        {
+            return ax * by - bx * ay;
+        }
+
+        public static bool IsVisible(Point s, Point e, Point p, int side)
+        {
+            double z = (p.X - s.X) * (e.Y - s.Y) - (e.X - s.X) * (p.Y - s.Y);
+            
+            // Point is on the left side
+            if (Math.Abs(z) < 1e-6)
+                return true;
+            return Math.Sign(side) == Math.Sign(z);
+        }
+
+
+        public static bool HasIntersection(Point s1, Point e1, Point s2, Point e2, out Point sec)
+        {
+            sec = Point.Empty;
+            
+            double resX = 0;
+            double resY = 0;
+
+            double kf, kg;
+
+            // Finding coefficients
+            if (s1.X - e1.X == 0)
+                kf = Double.PositiveInfinity;
+            else
+                kf = (double)(s1.Y - e1.Y) / (s1.X - e1.X);
+
+            if (s2.X - e2.X == 0)
+                kg = Double.PositiveInfinity;
+            else
+                kg = (double)(s2.Y - e2.Y) / (s2.X - e2.X);
+            
+            // Finding B
+            double bf = s1.Y - kf * s1.X;
+            double bg = s2.Y - kg * s2.X;
+
+            if (Math.Abs(kf - kg) < 1e-6)
+                return false;
+
+            resX = (bg - bf) / (kf - kg);
+            resY = kf * resX + bf;
+
+            sec = new Point((int)Math.Round(resX), (int)Math.Round(resY));
+
+            // Check if lies on first segment
+            bool correct = true;
+
+            double minX = Math.Min(s1.X, e1.X);
+            double maxX = Math.Max(s1.X, e1.X);
+
+            double minY = Math.Min(s1.Y, e1.Y);
+            double maxY = Math.Max(s1.Y, e1.Y);
+
+            correct &= resX >= minX && resX <= maxX && resY >= minY && resY <= maxY;
+
+            // Check if lies on second segment
+            minX = Math.Min(s2.X, e2.X);
+            maxX = Math.Max(s2.X, e2.X);
+
+            minY = Math.Min(s2.Y, e2.Y);
+            maxY = Math.Max(s2.Y, e2.Y);
+
+            correct &= resX >= minX && resX <= maxX && resY >= minY && resY <= maxY;
+
+            return correct;
+        }
+
+        public static List<Point> Cut(List<Point> polygonArg, List<Point> cutter)
+        {
+            List<Point> result = new List<Point>();
+            List<Point> polygon = new List<Point>(polygonArg);
+
+            if (polygon.Count < 4)
+            {
+                throw new ArgumentException("Слишком мало вершин в многоугольнике", nameof(polygon));
+            }
+
+            if (cutter.Count < 4)
+            {
+                throw new ArgumentException("Слишком мало вершин в отсекателе", nameof(cutter));
+            }
+
+            int dir = GetDirection(cutter);
+
+            if(dir == 0)
+                throw new ArgumentException("Отсекатель должен быть выпуклым", nameof(cutter));
+
+            polygon.RemoveAt(polygon.Count - 1);
+
+            // 1. 
+
+            // 2. Cycle foreach cutter's edge
+            Point s = polygon[0];
+            for (int i = 0; i < cutter.Count - 1; i++)
+            {
+                //nq = 0; // 2.1. Setting number of resulting vertexes to zero
+                result = new List<Point>();
+
+                Point f = Point.Empty;
+                // 2.2 Cycle foreach polygon's edge
+                for (int j = 0; j < polygon.Count; j++)
+                {
+                    // 2.2.1
+                    if (j == 0) // j == 1 (first vertex)
+                    {
+                        f = polygon[0];
+                        // goto 2.2.5
+                    }
+                    else
+                    {
+                        // 2.2.2 | 2.2.3
+                        Point t;
+                        bool intersected = HasIntersection(
+                            s,
+                            polygon[j],
+                            cutter[i],
+                            cutter[i + 1], out t);
+                        if (intersected)
+                        {
+                            // 2.2.4 Increasing number of resulting vertexes
+                            //nq++;
+                            result.Add(t);
+                        }
+                        // else goto 2.2.5
+                    }
+
+                    // 2.2.5
+                    s = polygon[j];
+
+                    // 2.2.6 Checking visibility of S against C_j C_j-1, adding S to result
+                    if (IsVisible(cutter[i], cutter[i + 1], s, -dir))
+                    {
+                        result.Add(s);
+                    }
+
+                    // 2.2.7 end of inner cycle
+                }
+
+                // 2.3 Checking if nq != 0
+                if (result.Count != 0)
+                {
+                    // 2.4
+                    Point t;
+                    bool intersected = HasIntersection(
+                        s,
+                        f,
+                        cutter[i],
+                        cutter[i + 1], out t);
+                    if (intersected)
+                    {
+                        // 2.6 Increasing number of resulting vertexes
+                        //nq++;
+                        result.Add(t);
+                    }
+                    else
+                    {
+                        // goto 2
+                    }
+                }
+                else
+                {
+                    polygon.Add(polygon[0]);
+                    return result;
+                }
+                // 2.7 ????
+                polygon = new List<Point>(result);
+                // 2.8
+            }
+
+            polygon.Add(polygon[0]);
+            result.Add(result[0]);
+            // 2.9
+            return result;
+        }
+
+    }
+}
---
+++ ComputerGraphicsVS/Lab_09/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_09
+{
+    public partial class Form1 : Form
+    {
+        private List<Point> _polygon = new List<Point>();
+        private List<Point> _cutter = new List<Point>();
+        private readonly Color _cutterColor = Color.Black;
+        private readonly Color _polygonColor = Color.Red;
+        private readonly Color _resultColor = Color.Green;
+
+        private bool _cutterFinalized = false;
+        private bool _polygonFinalized = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Point GetCursorRelativePosition()
+        {
+            Point absoluteCursor = System.Windows.Forms.Cursor.Position;
+            return DrawingCanvas.PointToClient(absoluteCursor);
+        }
+
+        private void DrawLine(Point a, Point b, Color color, int width)
+        {
+            Graphics gr = DrawingCanvas.CreateGraphics();
+            gr.DrawLine(new Pen(color, width), a, b);
+        }
+
+        private void ClearScreen()
+        {
+            DrawingCanvas.CreateGraphics().Clear(Color.White);
+        }
+
+        private void Redraw()
+        {
+            ClearScreen();
+            for (int i = 0; i < _polygon.Count - 1; i++)
+            {
+                DrawLine(_polygon[i], _polygon[i + 1], _polygonColor, 1);
+            }
+
+            for (int i = 0; i < _cutter.Count - 1; i++)
+            {
+                DrawLine(_cutter[i], _cutter[i + 1], _cutterColor, 1);
+            }
+        }
+
+        private void TestVisibility()
+        {
+            Point p = GetCursorRelativePosition();
+            int dir = Cutter.GetDirection(_cutter);
+            string msg = "";
+            for (int i = 0; i < _cutter.Count - 1; i++)
+            {
+                bool visible = Cutter.IsVisible(_cutter[i], _cutter[i + 1], p, -dir);
+                msg += i + ": " + visible + "\n";
+            }
+
+            HiddenLabel.Text = msg;
+        }
+
+        private void TestIntersection()
+        {
+            Graphics gr = DrawingCanvas.CreateGraphics();
+            for (int i = 0; i < _cutter.Count - 1; i++)
+            {
+                for (int j = 0; j < _polygon.Count - 1; j++)
+                {
+                    Point sec;
+                    bool flag = Cutter.HasIntersection(_cutter[i], _cutter[i + 1], _polygon[j], _polygon[j + 1],
+                        out sec);
+                    if (flag)
+                    {
+                        gr.FillEllipse(new SolidBrush(Color.Blue), sec.X - 5, sec.Y - 5, 10, 10);
+                    }
+                }
+            }
+        }
+
+        private void DrawingCanvas_Click(object sender, EventArgs e)
+        {
+            bool shiftPressed = (ModifierKeys & Keys.Shift) != 0;
+            MouseEventArgs mouseArgs = (MouseEventArgs) e;
+
+            if (mouseArgs.Button == MouseButtons.Middle)
+            {
+                if(!shiftPressed)
+                TestVisibility();
+                else
+                {
+                    TestIntersection();
+                }
+            }
+
+            if (mouseArgs.Button == MouseButtons.Left)
+            {
+                if(!_cutterFinalized)
+                    if (shiftPressed)
+                    {
+                        _cutterFinalized = true;
+                        DrawLine(_cutter.Last(), _cutter[0], _cutterColor, 1);
+                        _cutter.Add(_cutter[0]);
+                    }
+                    else
+                    {
+                        Point p = GetCursorRelativePosition();
+                        if (_cutter.Count > 0)
+                            DrawLine(_cutter.Last(), p, _cutterColor, 1);
+                        _cutter.Add(p);
+                    }
+                else
+                {
+                    if (!shiftPressed)
+                    {
+                        _cutterFinalized = false;
+                        _cutter.Clear();
+                        Redraw();
+                        Point p = GetCursorRelativePosition();
+                        _cutter.Add(p);
+                    }
+                }
+            }
+
+            if (mouseArgs.Button == MouseButtons.Right)
+                if (!_polygonFinalized)
+                    if (shiftPressed)
+                    {
+                        _polygonFinalized = true;
+                        DrawLine(_polygon.Last(), _polygon[0], _polygonColor, 1);
+                        _polygon.Add(_polygon[0]);
+                    }
+                    else
+                    {
+                        Point p = GetCursorRelativePosition();
+                        if (_polygon.Count > 0)
+                            DrawLine(_polygon.Last(), p, _polygonColor, 1);
+                        _polygon.Add(p);
+                    }
+                else
+                {
+                    if (!shiftPressed)
+                    {
+                        _polygonFinalized = false;
+                        _polygon.Clear();
+                        Redraw();
+                        Point p = GetCursorRelativePosition();
+                        _polygon.Add(p);
+                    }
+                }
+        }
+
+        private void proceedButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<Point> result = Cutter.Cut(_polygon, _cutter);
+
+                for (int i = 0; i < result.Count - 1; i++)
+                {
+                    DrawLine(result[i], result[i + 1], _resultColor, 2);
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                throw ex;
+            }
+            /*catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }*/
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            _polygon.Clear();
+            _cutter.Clear();
+            ClearScreen();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            colorPanelCutter.BackColor = _cutterColor;
+            colorPanelPolygon.BackColor = _polygonColor;
+            colorPanelResult.BackColor = _resultColor;
+        }
+    }
+}
